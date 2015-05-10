1555a0057e5a9993af618b5b5471c9ef1ef75ca8
横内宏樹 <yokouchi@yokouchihiroki-no-MacBook-Air.local>
2015-05-11 00:01:59 +0900

    user table add

--- scheme.sqlite3.sql
+++ scheme.sqlite3.sql
@@ -6,11 +6,13 @@ CREATE TABLE posts (
   created_at text NOT NUll,
   updated_at text NOT NULL
 );
-DROP TABLE IF EXISTS posts;
-CREATE TABLE posts (
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
   id INTEGER PRIMARY KEY NOT NULL,
-  nickname text NOT NULL,
-  body text NOT NULL,
+  userid text NOT NULL,
+  password text NOT NULL,
   created_at text NOT NUll,
   updated_at text NOT NULL
 );
+insert into user values(0, 'yokouchi', 'password', '2015-05-01 15:00:00', '2015-05-01 15:00:00');
